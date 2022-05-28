9869d50a251ed6080b64a352e2eb5fd037c79e31
Kevin Mason <krmason2020@gmail.com>
2022-05-27 21:59:43 -0700

    background stars working now

---
+++ src/components/Header.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<header>
+  <h1>THE PLANETS</h1>
+  <img src="./img/icon-hamburger.svg" alt="" />
+</header>
+
+<style>
+  img {
+    width: 20px;
+    height: 20px;
+  }
+  header {
+    background-color: black;
+  }
+</style>
