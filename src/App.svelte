a7f9c7166ab04c021c22f168eaa79a4a48925781
Kevin Mason <krmason2020@gmail.com>
2022-05-28 19:58:32 -0700

    made some comments on app architeture

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import Header from "./components/Header.svelte";
+  //parse the data store in object then seed header and initial planet
 </script>
 
 <Header />
---
+++ src/components/MenuListItems.svelte
@@ -0,0 +1,18 @@
+<script>
+  //need to iterate over json file
+  //and seed the list by planet name
+  //then on click send and event to change the view of main to show a planet
+</script>
+
+<ul>
+  <li>MERCURY</li>
+  <li>VENUS</li>
+  <li>EARTH</li>
+  <li>MARS</li>
+  <li>JUPITER</li>
+  <li>SATURN</li>
+  <li>URANUS</li>
+  <li>NEPTUNE</li>
+</ul>
+
+<style></style>
